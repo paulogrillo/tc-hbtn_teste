ecb762c6ea2921c59fe5d57bc96868d6cdc56ef4
Paulo Grillo <paulodigitalart@gmail.com>
2022-06-06 11:31:19 -0300

    Fix

--- 00x00/email/Pessoa.java
+++
@@ -1,9 +0,0 @@
-public class Pessoa {
-    public static boolean emailValid(String email) {
-        if (email.contains("@") || email.length() <= 50) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
